3243ccea74055f03b331fc19a7f97be0e23f9785
byjusmx3 <90639391+byjusmx3@users.noreply.github.com>
2021-10-11 22:15:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -30,8 +30,8 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
   
 function speak(){
   var synth = window.speechSynthesis;
-  speak_data_1 = "The first prediction is " + prediction_1;
-  speak_data_2 = "And the second prediction is " + prediction_2;
+  speak_data_1 = "La primera predicción es " + prediction_1;
+  speak_data_2 = "Y la segunda predicción es " + prediction_2;
   var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
   synth.speak(utterThis);
 }
